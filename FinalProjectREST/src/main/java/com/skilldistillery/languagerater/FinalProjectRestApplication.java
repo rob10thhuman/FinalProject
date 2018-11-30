118c85ddd4e04ba5181daf02eb3af0d3a36c12a0
ajtpxn <ajtpxn@gmail.com>
2018-11-30 12:42:22 -0700

    Updates to main

--- FinalProjectREST/src/main/java/com/skilldistillery/languagerater/FinalProjectRestApplication.java
+++ FinalProjectREST/src/main/java/com/skilldistillery/languagerater/FinalProjectRestApplication.java
@@ -2,11 +2,26 @@ package com.skilldistillery.languagerater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class FinalProjectRestApplication {
+public class FinalProjectRestApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FinalProjectRestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectRestApplication.class, args);
+	}
+	
+	@Bean
+	public PasswordEncoder configurePasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
