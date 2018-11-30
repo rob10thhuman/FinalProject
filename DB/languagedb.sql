12e1978cd079ed666a5b64c6166eb2c2cb8c87f4
ajtpxn <ajtpxn@gmail.com>
2018-11-30 12:28:47 -0700

    pushing db updates

--- DB/languagedb.sql
+++ DB/languagedb.sql
@@ -7,6 +7,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema languagedb
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `languagedb` ;
 
 -- -----------------------------------------------------
 -- Schema languagedb
@@ -15,11 +16,11 @@ CREATE SCHEMA IF NOT EXISTS `languagedb` DEFAULT CHARACTER SET utf8 ;
 USE `languagedb` ;
 
 -- -----------------------------------------------------
--- Table `languagedb`.`user`
+-- Table `user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `languagedb`.`user` ;
+DROP TABLE IF EXISTS `user` ;
 
-CREATE TABLE IF NOT EXISTS `languagedb`.`user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
@@ -33,11 +34,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `languagedb`.`language`
+-- Table `language`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `languagedb`.`language` ;
+DROP TABLE IF EXISTS `language` ;
 
-CREATE TABLE IF NOT EXISTS `languagedb`.`language` (
+CREATE TABLE IF NOT EXISTS `language` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `logo` VARCHAR(45) NULL,
@@ -49,11 +50,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `languagedb`.`comment`
+-- Table `comment`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `languagedb`.`comment` ;
+DROP TABLE IF EXISTS `comment` ;
 
-CREATE TABLE IF NOT EXISTS `languagedb`.`comment` (
+CREATE TABLE IF NOT EXISTS `comment` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `comment` VARCHAR(45) NULL,
   `date_added` DATETIME NULL,
@@ -65,23 +66,23 @@ CREATE TABLE IF NOT EXISTS `languagedb`.`comment` (
   INDEX `user_id_idx` (`user_id` ASC),
   CONSTRAINT `comment_user_id`
     FOREIGN KEY (`user_id`)
-    REFERENCES `languagedb`.`user` (`id`)
+    REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `comment_language_id`
     FOREIGN KEY (`language_id`)
-    REFERENCES `languagedb`.`language` (`id`)
+    REFERENCES `language` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `languagedb`.`language_rating`
+-- Table `language_rating`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `languagedb`.`language_rating` ;
+DROP TABLE IF EXISTS `language_rating` ;
 
-CREATE TABLE IF NOT EXISTS `languagedb`.`language_rating` (
+CREATE TABLE IF NOT EXISTS `language_rating` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `rating` INT NOT NULL,
   `user_id` INT NOT NULL,
@@ -91,23 +92,23 @@ CREATE TABLE IF NOT EXISTS `languagedb`.`language_rating` (
   INDEX `user_id_idx` (`user_id` ASC),
   CONSTRAINT `language_rating_user_id`
     FOREIGN KEY (`user_id`)
-    REFERENCES `languagedb`.`user` (`id`)
+    REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `language_rating_language_id`
     FOREIGN KEY (`language_id`)
-    REFERENCES `languagedb`.`language` (`id`)
+    REFERENCES `language` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `languagedb`.`vote`
+-- Table `vote`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `languagedb`.`vote` ;
+DROP TABLE IF EXISTS `vote` ;
 
-CREATE TABLE IF NOT EXISTS `languagedb`.`vote` (
+CREATE TABLE IF NOT EXISTS `vote` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `vote` TINYINT NOT NULL,
   `user_id` INT NOT NULL,
@@ -117,12 +118,12 @@ CREATE TABLE IF NOT EXISTS `languagedb`.`vote` (
   INDEX `user_id_idx` (`user_id` ASC),
   CONSTRAINT `vote_comment_id`
     FOREIGN KEY (`comment_id`)
-    REFERENCES `languagedb`.`comment` (`id`)
+    REFERENCES `comment` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `vote_user_id`
     FOREIGN KEY (`user_id`)
-    REFERENCES `languagedb`.`user` (`id`)
+    REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -133,8 +134,64 @@ GRANT USAGE ON *.* TO admin@localhost;
 SET SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
 
-GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE `languagedb`.* TO 'admin'@'localhost';
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'admin'@'localhost';
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `user`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `languagedb`;
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `first_name`, `last_name`, `active`, `role`) VALUES (1, 'rob', 'rob', 'rob@10thHuman.com', 'Rob', 'Thompson', 1, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `first_name`, `last_name`, `active`, `role`) VALUES (2, 'josh', 'josh', 'josh@josh.com', 'Josh', 'O', 1, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `first_name`, `last_name`, `active`, `role`) VALUES (3, 'henry', 'henry', 'henry@henry.com', 'Henry', 'Z', 1, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `first_name`, `last_name`, `active`, `role`) VALUES (4, 'brandon', 'brandon', 'brandon@brandon.com', 'Brandon', 'B', 1, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `language`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `languagedb`;
+INSERT INTO `language` (`id`, `name`, `logo`, `creator`, `year_created`, `info`) VALUES (1, 'java', NULL, NULL, NULL, NULL);
+INSERT INTO `language` (`id`, `name`, `logo`, `creator`, `year_created`, `info`) VALUES (2, 'python', NULL, NULL, NULL, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `comment`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `languagedb`;
+INSERT INTO `comment` (`id`, `comment`, `date_added`, `date_updated`, `user_id`, `language_id`) VALUES (1, 'hello world', NULL, NULL, 1, 1);
+INSERT INTO `comment` (`id`, `comment`, `date_added`, `date_updated`, `user_id`, `language_id`) VALUES (2, 'goodbye world', NULL, NULL, 1, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `language_rating`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `languagedb`;
+INSERT INTO `language_rating` (`id`, `rating`, `user_id`, `language_id`) VALUES (1, 3, 1, 1);
+INSERT INTO `language_rating` (`id`, `rating`, `user_id`, `language_id`) VALUES (2, 4, 1, 2);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `vote`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `languagedb`;
+INSERT INTO `vote` (`id`, `vote`, `user_id`, `comment_id`) VALUES (1, 2, 1, 1);
+INSERT INTO `vote` (`id`, `vote`, `user_id`, `comment_id`) VALUES (2, 3, 2, 1);
+
+COMMIT;
