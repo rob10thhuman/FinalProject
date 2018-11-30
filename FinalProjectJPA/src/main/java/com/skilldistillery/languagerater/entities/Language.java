9668045414df0da9eacaab9cefd3de006a9a9426
rob10thHuman <rob@10thHuman.com>
2018-11-30 11:14:12 -0700

    language entity flushed out and test entity passes db access

--- FinalProjectJPA/src/main/java/com/skilldistillery/languagerater/entities/Language.java
+++ FinalProjectJPA/src/main/java/com/skilldistillery/languagerater/entities/Language.java
@@ -1,5 +1,110 @@
 package com.skilldistillery.languagerater.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="language")
 public class Language {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id; 
+	
+	private String name; 
+	
+	private String logo; 
+	
+	private String creator; 
+	
+	@Column(name="year_created")
+	private String yrCreated; 
+	
+	private String info;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLogo() {
+		return logo;
+	}
+
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public String getYrCreated() {
+		return yrCreated;
+	}
+
+	public void setYrCreated(String yrCreated) {
+		this.yrCreated = yrCreated;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Language [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", logo=");
+		builder.append(logo);
+		builder.append(", creator=");
+		builder.append(creator);
+		builder.append(", yrCreated=");
+		builder.append(yrCreated);
+		builder.append(", info=");
+		builder.append(info);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	public Language(int id, String name, String logo, String creator, String yrCreated, String info) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.logo = logo;
+		this.creator = creator;
+		this.yrCreated = yrCreated;
+		this.info = info;
+	} 
+	
+	public Language () {
+		
+	}
+	
 
 }
