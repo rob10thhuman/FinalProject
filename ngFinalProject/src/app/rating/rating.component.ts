fc6b45313a5130d6e9d6f4746cbc1779dd323360
ajtpxn <ajtpxn@gmail.com>
2018-12-03 16:33:41 -0700

    Changes to rating component

--- ngFinalProject/src/app/rating/rating.component.ts
+++ ngFinalProject/src/app/rating/rating.component.ts
@@ -1,5 +1,6 @@
 import { Rating } from './../models/rating';
 import { Component, OnInit } from '@angular/core';
+import { DetailLanguageComponent } from '../detail-language/detail-language.component';
 
 @Component({
   selector: 'app-rating',
@@ -8,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingComponent implements OnInit {
 
-  constructor() { }
+  avgRating = 0;
+
+  constructor(private detail: DetailLanguageComponent) { }
 
   ngOnInit() {
+    this.showRatings();
+  }
+
+  showRatings () {
+    const ratings = this.detail.language.lRatings;
+    console.log(ratings);
+    let count = 0;
+    let sum = 0;
+    console.log('ratings.length');
+    console.log(ratings.length);
+    for (let i = 0; i < ratings.length; i++) {
+      console.log('i');
+      console.log(i);
+      count++;
+      // this.avgRating = this.avgRating + this.detail.language.lRatings[i].rating;
+      sum += this.detail.language.lRatings[i].rating;
+      console.log('count');
+      console.log(count);
+    }
+    this.avgRating = count === 0 ? 0 : sum / count;
+    console.log(this.avgRating);
+    return this.avgRating;
   }
 
 }
